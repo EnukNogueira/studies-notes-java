e6c8befd63ef33ac6b17a240bd88d44bbbc0a2d9
Enuk <enuk.santos@gmail.com>
2025-03-27 19:04:44 -0300

    Add files via upload

---
+++ Entrada_De_Dados/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Entrada_De_Dados {
+}
---
+++ teste-processamento/src/teste1/teste.java
@@ -0,0 +1,19 @@
+package teste1;
+
+public class teste {
+
+	public static void main(String[] args) {
+
+		int a, b;
+		double resultado;
+		
+		a = 5;
+		b = 2;
+		
+		resultado = (double) a / b;
+		
+		System.out.println(resultado);
+
+	}
+	
+}

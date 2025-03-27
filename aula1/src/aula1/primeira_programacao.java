34bfd820608007975d0bb2e92309c7c2fc22a76f
Enuk <enuk.santos@gmail.com>
2025-03-27 19:04:00 -0300

    Update primeira_programacao.java

--- aula1/src/aula1/primeira_programacao.java
+++ aula1/src/aula1/primeira_programacao.java
@@ -1,10 +1,21 @@
 package aula1;
 
+import java.util.Locale;
+
 public class primeira_programacao {
 
 	public static void main(String[] args) {
-		System.out.println("Ola mundo");
+		
+		String nome = "Maria";
+		int idade = 32;
+		double renda = 4000.0;
 
+		System.out.println(nome);
+		System.out.println(idade);
+		System.out.println(renda);
+		System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n ", nome, idade, renda);;
+		Locale.setDefault(Locale.US);
+		
 	}
 
 }
